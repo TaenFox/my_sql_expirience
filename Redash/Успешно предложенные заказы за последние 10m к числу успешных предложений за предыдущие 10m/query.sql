70396ff03a29c5d08cba34408f8fc619e4b29ef4
Павел Мокеев <mokeev.pavel@gmail.com>
2022-03-18 14:44:43 +0300

    Добавил запрос по алерту

---
+++ Redash/Успешно предложенные заказы за последние 10m к числу успешных предложений за предыдущие 10m/query.sql
@@ -0,0 +1,26 @@
+SELECT  s.k AS `Коэффициент`,
+        if(s.k>=1,'Увеличение количества заказов',
+            if(s.k<0.7,
+              '!Критическое уменьшение количества заказов!',
+              'Уменьшение количества заказов')
+          ) AS `Оценка`
+FROM
+  (SELECT
+      (
+             (SELECT count(*)
+              FROM fairbot_suggests_success
+              WHERE date_suggest
+                BETWEEN
+                  NOW() - interval 10 MINUTE
+                AND
+                  NOW())
+              /
+             (SELECT count(*)
+              FROM fairbot_suggests_success
+              WHERE date_suggest
+                BETWEEN
+                  NOW() - interval 20 MINUTE
+                AND
+                  NOW() - interval 10 MINUTE)
+      )
+  AS 'k') AS s
